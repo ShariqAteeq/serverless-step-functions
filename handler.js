6ea72a439685a66bb8e464166511cdacd9d0f71a
ShariqAteeq <m.shariq22@hotmail.com>
2022-01-31 22:45:57 +0500

    minor fixes

--- handler.js
+++ handler.js
@@ -1,7 +1,33 @@
 "use strict";
 
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.checkInventory = async (event) => {
-  return "Stock Available";
+  const { bookId, quantity } = event;
+
+  try {
+    const bookRes = await dynamodb
+      .get("stepfunction-books", { bookId })
+      .promise();
+    const book = bookRes.Item;
+
+    if (isBookAvailable(book, quantity)) {
+      return book;
+    } else {
+      let bookOutOfStockError = new Error("The Book is out of stock");
+      bookOutOfStockError.name = "BookOutOfStock";
+      throw bookOutOfStockError;
+    }
+  } catch (error) {
+    if(error.name === "BookOutOfStock") {
+       throw error;
+    } else {
+      let bookNotFoundError = new Error("The Book is out of stock");
+      bookNotFoundError.name = "BookNotFound";
+      throw bookNotFoundError;
+    }
+  }
 };
 module.exports.calculateTotal = async (event) => {
   return 100;
