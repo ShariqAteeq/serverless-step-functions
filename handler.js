551833c4b17ba30e8359044903e24cf4d888ab70
ShariqAteeq <m.shariq22@hotmail.com>
2022-02-03 06:34:07 +0500

    working

--- handler.js
+++ handler.js
@@ -3,12 +3,16 @@
 const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
+const isBookAvailable = (book, quantity) => {
+  return book.quantity - quantity > 0;
+};
+
 module.exports.checkInventory = async (event) => {
   const { bookId, quantity } = event;
 
   try {
     const bookRes = await dynamodb
-      .get("stepfunction-books", { bookId })
+      .get({ TableName: "stepfunction-books", Key: { bookId } })
       .promise();
     const book = bookRes.Item;
 
@@ -20,8 +24,8 @@ module.exports.checkInventory = async (event) => {
       throw bookOutOfStockError;
     }
   } catch (error) {
-    if(error.name === "BookOutOfStock") {
-       throw error;
+    if (error.name === "BookOutOfStock") {
+      throw error;
     } else {
       let bookNotFoundError = new Error("The Book is out of stock");
       bookNotFoundError.name = "BookNotFound";
@@ -29,6 +33,35 @@ module.exports.checkInventory = async (event) => {
     }
   }
 };
-module.exports.calculateTotal = async (event) => {
-  return 100;
+module.exports.calculateTotal = async ({ book, quantity }) => {
+  let total = book.price * quantity;
+  return { total };
+};
+
+const deductPoints = async (userId) => {
+  await dynamodb.update({
+    TableName: "stepfunction-users",
+    Key: { userId },
+    UpdateExpression: "SET points = :zero",
+    
+  })
+}
+
+module.exports.redeemPoints = async ({ userId, total }) => {
+  let orderTotal = total.total;
+  const userRes = await dynamodb
+    .get({ TableName: "stepfunction-users", Key: { userId } })
+    .promise();
+  const user = userRes.Item;
+
+  const points = user.points;
+  if (orderTotal > points) {
+    await deductPoints(userId);
+  } else {
+    throw new Error("Order total is less than redeem points");
+  }
+};
+module.exports.billCustomer = async ({ book, quantity }) => {
+  let total = book.price * quantity;
+  return { total };
 };
