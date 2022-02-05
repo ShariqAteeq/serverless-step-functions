7fb2b6573a727845d7d4e923de7a4bcc86c7d18a
ShariqAteeq <m.shariq22@hotmail.com>
2022-02-05 23:32:04 +0500

    finalized

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@
 
 const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const StepFunction = new AWS.StepFunctions();
 
 const isBookAvailable = (book, quantity) => {
   return book.quantity - quantity > 0;
@@ -27,7 +28,7 @@ module.exports.checkInventory = async (event) => {
     if (error.name === "BookOutOfStock") {
       throw error;
     } else {
-      let bookNotFoundError = new Error("The Book is out of stock");
+      let bookNotFoundError = new Error("The Book is not found");
       bookNotFoundError.name = "BookNotFound";
       throw bookNotFoundError;
     }
@@ -39,29 +40,113 @@ module.exports.calculateTotal = async ({ book, quantity }) => {
 };
 
 const deductPoints = async (userId) => {
-  await dynamodb.update({
-    TableName: "stepfunction-users",
-    Key: { userId },
-    UpdateExpression: "SET points = :zero",
-    
-  })
-}
+  await dynamodb
+    .update({
+      TableName: "stepfunction-users",
+      Key: { userId },
+      UpdateExpression: "SET points = :zero",
+      ExpressionAttributeValues: {
+        ":zero": 0,
+      },
+    })
+    .promise();
+};
 
 module.exports.redeemPoints = async ({ userId, total }) => {
-  let orderTotal = total.total;
-  const userRes = await dynamodb
-    .get({ TableName: "stepfunction-users", Key: { userId } })
-    .promise();
-  const user = userRes.Item;
+  try {
+    let orderTotal = total.total;
+    const userRes = await dynamodb
+      .get({ TableName: "stepfunction-users", Key: { userId } })
+      .promise();
+    const user = userRes.Item;
 
-  const points = user.points;
-  if (orderTotal > points) {
-    await deductPoints(userId);
-  } else {
-    throw new Error("Order total is less than redeem points");
+    const points = user.points;
+    if (orderTotal > points) {
+      await deductPoints(userId);
+      return { total: orderTotal, points };
+    } else {
+      throw new Error("Order total is less than redeem points");
+    }
+  } catch (error) {
+    throw new Error(error);
   }
 };
-module.exports.billCustomer = async ({ book, quantity }) => {
-  let total = book.price * quantity;
-  return { total };
+module.exports.billCustomer = async (params) => {
+  return "Successfully Billed!";
 };
+module.exports.restoreRedeemPoints = async ({ userId, total }) => {
+  console.log("total", total);
+
+  try {
+    if (total.points) {
+      await dynamodb
+        .update({
+          TableName: "stepfunction-users",
+          Key: { userId },
+          UpdateExpression: "SET points = :points",
+          ExpressionAttributeValues: {
+            ":points": total.points,
+          },
+        })
+        .promise();
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const updateBookQuantity = async (bookId, orderQuantity) => {
+  console.log("bookId: ", bookId);
+  console.log("orderQuantity: ", orderQuantity);
+  let params = {
+      TableName: 'bookTable',
+      Key: { 'bookId': bookId },
+      UpdateExpression: 'SET quantity = quantity - :orderQuantity',
+      ExpressionAttributeValues: {
+          ':orderQuantity': orderQuantity
+      }
+  };
+  await dynamodb.update(params).promise();
+}
+
+module.exports.sqsWorker = async (event) => {
+  try {
+      console.log(JSON.stringify(event));
+      let record = event.Records[0];
+      var body = JSON.parse(record.body);
+      /** Find a courier and attach courier information to the order */
+      let courier = "<courier email>";
+
+      // update book quantity
+      await updateBookQuantity(body.Input.bookId, body.Input.quantity);
+
+     // throw "Something wrong with Courier API";
+
+      // Attach curier information to the order
+      await StepFunction.sendTaskSuccess({
+          output: JSON.stringify({ courier }),
+          taskToken: body.Token
+      }).promise();
+  } catch (e) {
+      console.log("===== You got an Error =====");
+      console.log(e);
+      await StepFunction.sendTaskFailure({
+          error: "NoCourierAvailable",
+          cause: "No couriers are available",
+          taskToken: body.Token
+      }).promise();
+  }
+}
+
+module.exports.restoreQuantity = async ({ bookId, quantity }) => {
+  let params = {
+      TableName: 'bookTable',
+      Key: { bookId: bookId },
+      UpdateExpression: 'set quantity = quantity + :orderQuantity',
+      ExpressionAttributeValues: {
+          ':orderQuantity': quantity
+      }
+  };
+  await dynamodb.update(params).promise();
+  return "Quantity restored"
+}
